a12c6c570c9984f371b9707a0266e5b651f09a74
mpoeter <manuel@manuel-poeter.at>
2022-01-24 11:39:04 +0100

    Add pop with optional result and support for non-default constructible types to michael_scott_queue.

--- test/michael_scott_queue_test.cpp
+++ test/michael_scott_queue_test.cpp
@@ -6,6 +6,8 @@
 #include <xenium/reclamation/quiescent_state_based.hpp>
 #include <xenium/reclamation/stamp_it.hpp>
 
+#include "helpers.hpp"
+
 #include <gtest/gtest.h>
 
 #include <thread>
@@ -29,6 +31,18 @@ using Reclaimers =
                    xenium::reclamation::debra<>::with<xenium::policy::scan_frequency<10>>>;
 TYPED_TEST_SUITE(MichaelScottQueue, Reclaimers);
 
+TYPED_TEST(MichaelScottQueue, try_pop_from_empty_queue) {
+  xenium::michael_scott_queue<int, xenium::policy::reclaimer<TypeParam>> queue;
+  int elem = 0;
+  ASSERT_FALSE(queue.try_pop(elem));
+}
+
+TYPED_TEST(MichaelScottQueue, pop_from_empty_queue) {
+  xenium::michael_scott_queue<int, xenium::policy::reclaimer<TypeParam>> queue;
+  auto elem = queue.pop();
+  ASSERT_FALSE(elem.has_value());
+}
+
 TYPED_TEST(MichaelScottQueue, push_try_pop_returns_pushed_element) {
   xenium::michael_scott_queue<int, xenium::policy::reclaimer<TypeParam>> queue;
   queue.push(42);
@@ -37,16 +51,24 @@ TYPED_TEST(MichaelScottQueue, push_try_pop_returns_pushed_element) {
   EXPECT_EQ(42, elem);
 }
 
+TYPED_TEST(MichaelScottQueue, push_pop_returns_pushed_element) {
+  xenium::michael_scott_queue<int, xenium::policy::reclaimer<TypeParam>> queue;
+  queue.push(42);
+  auto elem = queue.pop();
+  ASSERT_TRUE(elem.has_value());
+  EXPECT_EQ(42, *elem);
+}
+
 TYPED_TEST(MichaelScottQueue, push_two_items_pop_them_in_FIFO_order) {
   xenium::michael_scott_queue<int, xenium::policy::reclaimer<TypeParam>> queue;
   queue.push(42);
   queue.push(43);
-  int elem1 = 0;
-  int elem2 = 0;
-  EXPECT_TRUE(queue.try_pop(elem1));
-  EXPECT_TRUE(queue.try_pop(elem2));
-  EXPECT_EQ(42, elem1);
-  EXPECT_EQ(43, elem2);
+  auto elem1 = queue.pop();
+  auto elem2 = queue.pop();
+  EXPECT_TRUE(elem1.has_value());
+  EXPECT_TRUE(elem2.has_value());
+  EXPECT_EQ(42, *elem1);
+  EXPECT_EQ(43, *elem2);
 }
 
 TYPED_TEST(MichaelScottQueue, supports_move_only_types) {
@@ -57,6 +79,39 @@ TYPED_TEST(MichaelScottQueue, supports_move_only_types) {
   ASSERT_TRUE(queue.try_pop(elem));
   ASSERT_NE(nullptr, elem);
   EXPECT_EQ(42, *elem);
+}
+
+TYPED_TEST(MichaelScottQueue, supports_non_default_constructible_types) {
+  xenium::michael_scott_queue<xenium::test::non_default_constructible, xenium::policy::reclaimer<TypeParam>> queue;
+  queue.push(xenium::test::non_default_constructible(42));
+
+  auto elem = queue.pop();
+  ASSERT_TRUE(elem.has_value());
+  EXPECT_EQ(42, elem->value);
+}
+
+TYPED_TEST(MichaelScottQueue, correctly_destroys_stored_objects) {
+  int created = 0;
+  int destroyed = 0;
+  struct Counting {
+    Counting(int& created, int& destroyed) : created(created), destroyed(destroyed) { ++created; }
+    Counting(const Counting& r) noexcept : created(r.created), destroyed(r.destroyed) { ++created; }
+    ~Counting() { ++destroyed; }
+    int& created;
+    int& destroyed;
+  };
+  {
+    xenium::michael_scott_queue<Counting, xenium::policy::reclaimer<TypeParam>> queue;
+    queue.push({created, destroyed});
+    queue.push({created, destroyed});
+    queue.push({created, destroyed});
+    queue.push({created, destroyed});
+
+    EXPECT_TRUE(queue.pop());
+    EXPECT_TRUE(queue.pop());
+    EXPECT_EQ(2, created - destroyed);
+  }
+  EXPECT_EQ(0, created - destroyed);
 }
 
 TYPED_TEST(MichaelScottQueue, parallel_usage) {
