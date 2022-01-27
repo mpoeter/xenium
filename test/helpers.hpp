97012c0cf10cfa36c563a0858909a346d2c58ea6
mpoeter <manuel@manuel-poeter.at>
2022-01-27 16:26:47 +0100

    Add pop with optional result and support for non-default constructible types to nikolaev_queue.

--- test/helpers.hpp
+++ test/helpers.hpp
@@ -5,4 +5,10 @@ struct non_default_constructible {
   explicit non_default_constructible(int v) : value(v) {}
   int const value;
 };
+
+struct non_default_constructible_assignable {
+  explicit non_default_constructible_assignable(int v) : value(v) {}
+  int value;
+};
+
 } // namespace xenium::test
